f25e34bc2d9388f7ac6c9276d08e7e21f26fc0ad
Ernest Bruno, Panatda Inthala and Tyler Kostelak <panatda@>
2018-01-29 11:36:28 -0800

    Add toggleClass to js when user clicks button

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $("#btnBegin").click(function(){
+    $("#petsContainer").toggleClass("hidden");
+    $("#landingPage").toggleClass("hidden");
+  });
+  $("#btnDog").click(function(){
+    $("#dogContainer").toggleClass("hidden");
+  });
+  $("#btnCat").click(function(){
+    $("#catContainer").toggleClass("hidden");
+  });
+  $("#dogForm").submit(function(event){
+    event.preventDefault();
+    $("#cartContainer").toggleClass("hidden");
+    $("#dogContainer").toggleClass("hidden");
+  });
+  $("#catForm").submit(function(event){
+    event.preventDefault();
+    $("#cartContainer").toggleClass("hidden");
+    $("#catContainer").toggleClass("hidden");
+  });
+});
